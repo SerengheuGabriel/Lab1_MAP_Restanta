ccd0f998d810fce85e46f54c1d208ec29b0b024d
FreddiesPizza <gabrielserengheu@gmail.com>
2022-10-04 11:02:21 +0300

    changed variable names

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) {
-        int s = 0;
+        int sumInts = 0;
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Enter number of integers: ");
-        int n = keyboard.nextInt();
-        for(int i = 0; i<n; i++) {
-            s += keyboard.nextInt();
+        int numberInts = keyboard.nextInt();
+        for(int i = 0; i<numberInts; i++) {
+            sumInts += keyboard.nextInt();
         }
-        System.out.println(s / n);
+        System.out.println(sumInts / numberInts);
     }
 }
